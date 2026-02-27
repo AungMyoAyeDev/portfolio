ce84edf90881c05daa80aa3adda99dd1c8e1546a
Aung Myo Aye <aungmyoaye101@gmail.com>
2026-02-27 22:08:39 +0630

    feat : add email send service and add resend package.

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -9,13 +9,18 @@ import {
     FieldLabel,
 } from "@/src/components/ui/field"
 import { sendEmail } from "../lib/send-email";
+import { useTransition } from "react";
 
 const ContactForm = () => {
-
+    const [isPending, startTransition] = useTransition();
     const handleSubmit = (formData: FormData) => {
-        console.log(formData)
-        sendEmail(formData)
+        startTransition(() => {
+            sendEmail(formData)
+        })
+
+
     }
+    console.log(isPending)
     return (
         <Card className=" rounded-2xl shadow-xl">
             <CardContent >
@@ -25,7 +30,11 @@ const ContactForm = () => {
                     <FieldGroup>
                         <Field>
                             <FieldLabel htmlFor="name">Name</FieldLabel>
-                            <Input id="name" name="name" placeholder="Your name" required />
+                            <Input
+                                id="name"
+                                name="name"
+                                placeholder="Your name"
+                                required />
                         </Field>
                         <Field>
                             <FieldLabel htmlFor="email">Email</FieldLabel>
--- src/lib/send-email.ts
+++ src/lib/send-email.ts
@@ -1,4 +1,7 @@
 "use server";
+import { Resend } from "resend";
+
+
 export type FormState = {
     success: boolean,
     message: string
@@ -6,10 +9,27 @@ export type FormState = {
 export const sendEmail = async (formData: FormData): Promise<FormState> => {
     console.log(formData, "In ")
     try {
-        const name = formData.get("name")
-        const email = formData.get("email")
-        const message = formData.get("message")
+        const api_key = process.env.RESEND_API_KEY;
+        console.log(api_key)
+        const resend = new Resend(api_key);
+        const name = formData.get("name") as string;
+        const email = formData.get("email") as string;
+        const message = formData.get("message") as string;
         console.log(name, email, message)
+        const success = await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: "aungmyoaye101@gmail.com",
+            subject: `New message from ${name}`,
+            replyTo: email,
+            html: `
+        <h2>New Contact Message</h2>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong></p>
+        <p>${message}</p>
+      `,
+        });
+        console.log(success)
         return { success: true, message: "Message sent! We'll be in touch." };
     } catch (error) {
         return { success: false, message: "Failed to send message. Try again later." };
