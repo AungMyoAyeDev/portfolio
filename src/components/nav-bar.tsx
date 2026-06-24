71e21da5554ed9a1d8bf4b8ba35b3b810007495c
Aung Myo Aye <aungmyoayedev@gmail.com>
2026-06-24 21:57:15 +0630

    update : github link.

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -7,98 +7,74 @@ import ThemeToggle from "./ui/theme-toggle";
 import { Button } from "@/src/components/ui/button";
 import { Handshake } from "lucide-react";
 
-const NAV_LINKS = [
-    "about", "projects", "contact"
-]
+const NAV_LINKS = ["home", "about", "projects", "contact"];
 
 export default function Navbar() {
-    const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setIsScrolled(window.scrollY > 20);
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 20);
+    };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-    return (
-        <header
-            className=" max-w-7xl mx-auto fixed top-1 left-0 right-0 z-50 px-4"
-        >
-            <nav
-                className={`
+  return (
+    <header className=" max-w-7xl mx-auto fixed top-1 left-0 right-0 z-50 px-4">
+      <nav
+        className={`
                rounded-full py-1.5 px-4 border border-border 
                 transition-all duration-300 flex gap-4 justify-between items-center 
-        ${isScrolled
-                        ? "bg-card/70 backdrop-blur-xl  shadow-sm shadow-accent/10"
-                        : "bg-card"
-                    }`}
+        ${
+          isScrolled
+            ? "bg-card/70 backdrop-blur-xl  shadow-sm shadow-accent/10"
+            : "bg-card"
+        }`}
+      >
+        {/* Brand */}
+        <div>
+          <Link href="#" className="flex items-center gap-1 ">
+            <Image
+              src={"/aungmyoaye.jpg"}
+              alt="aung myo aye photo"
+              width={40}
+              height={40}
+              className="rounded-full object-cover w-10 h-10"
+            />
+            <span className="text-primary  font-bold text-lg   uppercase hidden md:block">
+              Aung Myo AYe
+            </span>
+          </Link>
+        </div>
+
+        {/* Center Navigation */}
+        <div className="hidden md:flex gap-2 items-center">
+          {NAV_LINKS.map((item) => (
+            <Button
+              asChild
+              variant="outline"
+              size={"sm"}
+              key={item}
+              className="rounded-full capitalize"
             >
-                {/* Brand */}
-                <div >
-                    <Link href="#" className="flex items-center gap-1 ">
-                        <Image
-                            src={'/aungmyoaye.jpg'}
-                            alt="aung myo aye photo"
-                            width={40}
-                            height={40}
-                            className="rounded-full object-cover w-10 h-10"
-                        />
-                        <span
-                            className="text-primary  font-bold text-lg   uppercase hidden md:block"
-                        >Aung Myo AYe
-                        </span>
+              <Link href={`#${item}`}>{item}</Link>
+            </Button>
+          ))}
+        </div>
 
-
-                    </Link>
-                </div>
-
-                {/* Center Navigation */}
-                <div className="hidden md:flex gap-2 items-center">
-                    {
-                        NAV_LINKS.map(item => (
-                            <Button
-                                asChild
-                                variant='outline'
-                                size={'sm'}
-                                key={item}
-                                className="rounded-full capitalize"
-                            >
-                                <Link
-                                    href={`#${item}`}
-                                >
-                                    {item}
-                                </Link>
-
-
-                            </Button>
-
-                        ))
-                    }
-
-                </div>
-
-                {/* Right Side */}
-                <div className="flex items-center gap-2">
-                    <ThemeToggle />
-                    <Button
-                        className=" group rounded-full"
-                        asChild
-                    >
-                        <Link href={"mailto:aungmyoayedev@gmail.com"}>
-                            <Handshake />
-                            <span className="hidden sm:inline">
-                                Let&apos;s contact
-                            </span>
-
-                        </Link>
-
-                    </Button>
-                </div>
-            </nav>
-        </header>
-
-    );
+        {/* Right Side */}
+        <div className="flex items-center gap-2">
+          <ThemeToggle />
+          <Button className=" group rounded-full" asChild>
+            <Link href={"mailto:aungmyoayedev@gmail.com"}>
+              <Handshake />
+              <span className="hidden sm:inline">Let&apos;s contact</span>
+            </Link>
+          </Button>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,74 +1,75 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-import { Facebook, Github, LayoutPanelLeft, Linkedin, Server, Settings, } from "lucide-react";
-
-
+import {
+  Facebook,
+  Github,
+  LayoutPanelLeft,
+  Linkedin,
+  Server,
+  Settings,
+} from "lucide-react";
 
 export const SOCIAL_LINKS = [
   {
     name: "facebook",
     icon: Facebook,
-    link: "https://www.facebook.com/aung.myo.aye.294573"
+    link: "https://www.facebook.com/aung.myo.aye.294573",
   },
   {
     name: "linkedIn",
     icon: Linkedin,
-    link: "https://www.linkedin.com/in/aung-myo-aye-4b88143b4"
+    link: "https://www.linkedin.com/in/aung-myo-aye-4b88143b4",
   },
   {
     name: "github",
     icon: Github,
-    link: "https://github.com/AungMyoAye101"
+    link: "https://github.com/AungMyoAyeDev",
   },
 ];
 
 export const TECH_STACKS = [
   {
-    name: 'Frontend',
-    description: 'Building responsive and interactive user interfaces.',
+    name: "Frontend",
+    description: "Building responsive and interactive user interfaces.",
     icon: LayoutPanelLeft,
     techStack: [
-
-      { name: 'JavaScript', icon: '/icons/js.svg' },
+      { name: "JavaScript", icon: "/icons/js.svg" },
       { name: "TypeScript", icon: "/icons/typescript.svg" },
-      { name: 'React', icon: '/icons/react.svg', },
-      { name: 'Next.js', icon: 'icons/nextjs.svg', },
-      { name: "Css", icon: '/icons/css3.svg' },
-      { name: 'Tailwind CSS', icon: '/icons/tailwindcss.svg', },
-
-    ]
+      { name: "React", icon: "/icons/react.svg" },
+      { name: "Next.js", icon: "icons/nextjs.svg" },
+      { name: "Css", icon: "/icons/css3.svg" },
+      { name: "Tailwind CSS", icon: "/icons/tailwindcss.svg" },
+    ],
   },
 
   {
-    name: 'Backend',
-    description: 'Designing secure APIs and scalable server architecture.',
+    name: "Backend",
+    description: "Designing secure APIs and scalable server architecture.",
     icon: Server,
     techStack: [
-      { name: 'Node.js', icon: '/icons/nodejs.svg', },
-      { name: 'Express', icon: '/icons/expressjs.svg', },
+      { name: "Node.js", icon: "/icons/nodejs.svg" },
+      { name: "Express", icon: "/icons/expressjs.svg" },
       { name: "NestJs", icon: "/icons/nestjs.svg" },
-      { name: 'MongoDB', icon: '/icons/mongodb.svg', },
-      { name: 'PostgreSQL', icon: '/icons/postgresql.svg' },
+      { name: "MongoDB", icon: "/icons/mongodb.svg" },
+      { name: "PostgreSQL", icon: "/icons/postgresql.svg" },
       { name: "Prisma", icon: "/icons/prisma.svg" },
-
-    ]
+    ],
   },
   {
-    name: 'DevOps & Tools',
-    description: 'Deployment, testing and production optimization.',
+    name: "DevOps & Tools",
+    description: "Deployment, testing and production optimization.",
     icon: Settings,
     techStack: [
       { name: "Git", icon: "/icons/git.svg" },
       { name: "Tanstack Query", icon: "/icons/tanstack.svg" },
       { name: "Jest", icon: "/icons/jest.svg" },
-      // { name: "Docker", icon: "/icons/docker.svg" },
+      { name: "Docker", icon: "/icons/docker.svg" },
       // { name: "Sentry", icon: "/icons/sentry.svg" },
-    ]
-  }
+    ],
+  },
 ];
-
